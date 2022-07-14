1c4548218c494e8e54ae3c79f85f2d86fb76784f
WilkerSebastian <wilkerafonso2005@gmail.com>
2022-07-13 23:01:26 -0400

    return version

--- src/app/config/database.js
+++ src/app/config/database.js
@@ -1,21 +1,17 @@
-const Sequelize = require('sequelize');
-
-sequelize = new Sequelize(process.env.DATABASE_URL, {
+module.exports = {
+    dialect: 'postgres',
+    host: process.env.DATABASE_URL.slice(91 , 131),
+    username: process.env.DATABASE_URL.slice(11 , 25),
+    password: process.env.DATABASE_URL.slice(26 , 90),
+    database: process.env.DATABASE_URL.slice(137),
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
     dialectOptions: {
         ssl: {
-            require: true,
-            rejectUnauthorized: false
+          require: true,
+          rejectUnauthorized: false
         }
     }
-}
-);
-
-sequelize.authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
-
-module.exports = sequelize
+};
