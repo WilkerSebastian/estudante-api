a4929c0f321fd19f27d0b701fd8cbb57c78dc268
WilkerSebastian <wilkerafonso2005@gmail.com>
2022-07-13 22:38:22 -0400

    init heroku

--- src/app/config/database.js
+++ src/app/config/database.js
@@ -1,11 +1,17 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'postgres',
-    database: 'postgres',
+    host: process.env.DATABASE_URL.slice(91 , 131),
+    username: process.env.DATABASE_URL.slice(11 , 25),
+    password: process.env.DATABASE_URL.slice(26 , 90),
+    database: process.env.DATABASE_URL.slice(137),
     define: {
         timestamps: true,
         underscored: true,
     },
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+    }
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const App = require("./App")
 
 require("./app/database")
